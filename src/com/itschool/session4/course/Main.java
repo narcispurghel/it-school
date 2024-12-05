465eecce4a755b608385bb7e5f36b7f9131da15a
Narcis Purghel <narcispurghel@gmail.com>
2024-12-05 19:46:11 +0200

    add Salut

--- src/com/itschool/session4/course/Main.java
+++ src/com/itschool/session4/course/Main.java
@@ -3,5 +3,6 @@ package com.itschool.session4.course;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Salut!");
     }
 }
